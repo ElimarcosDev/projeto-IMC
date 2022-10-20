38a310389a000f08883636b120453b4b97bdad2d
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-10-20 14:39:29 -0300

    projeto finalizado

--- script/code.js
+++ script/code.js
@@ -5,11 +5,26 @@ function clique(){
     var resultado = document.getElementById('resultado')
     var containerimc = document.getElementById('containerimc')
     var imc = Number (peso) / ( Number (altura) * Number (altura));
-    resuimc.innerHTML = `Seu IMC é ${imc}`
-    containerimc.style.display = 'block'
-    if (imc > 18,5){
-        
     
+    containerimc.style.display = 'block'
+    if (imc < 18.5){
+        resuimc.innerHTML = `Seu IMC é ${imc.toFixed(1)} <Br> Você esta abaixo do peso!`
+    
+    }
+    else if (imc > 18.5 && imc < 25){
+        resuimc.innerHTML = `Seu IMC é ${imc.toFixed(1)} <Br> Você esta no peso ideal!`
+    }
+    else if (imc >= 25 && imc < 30){
+        resuimc.innerHTML = `Seu IMC é ${imc.toFixed(1)} <Br> Você esta levemente acima do peso!`
+    }
+    else if (imc >= 30 && imc < 35){
+        resuimc.innerHTML = `Seu IMC é ${imc.toFixed(1)} <Br> Você esta com obesidade grau I!`
+    }
+    else if (imc >= 35 && imc < 40){
+        resuimc.innerHTML = `Seu IMC é ${imc.toFixed(1)} <Br> Você esta com obesidade grau II (severa)!`
+    }
+    else if (imc > 40){
+        resuimc.innerHTML = `Seu IMC é ${imc.toFixed(1)} <Br> Você esta com obesidade grau III (mórbida)!`
     }
     
 }
